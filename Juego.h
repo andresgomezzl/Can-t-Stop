4a49cf22ece79d5d3806aa781b80f298f01627e0
Roxxx-Code <manuelariveragomez3@gmail.com>
2024-11-04 16:21:55 -0500

    Update Juego.h

--- Juego.h
+++ Juego.h
@@ -1,30 +1,14 @@
 #ifndef JUEGO_H
 #define JUEGO_H
-#include <iostream>
+
 #include "Tablero.h"
 #include "Jugadores.h"
 #include "Fichas.h"
 #include "Dados.h"
 
-using namespace std;
-
-class Juego { 
+class Juego {
 public:
-    void prueba() {
-        Tablero t;
-        Jugadores j;
-        Fichas f;
-        Dados d;
-
-        
-        j.bienvenida(); //Inicio del juego, pide cantidad jugadores, pide nombres 
-        f.elegirColores(j);
-        t.definirTablero();
-        t.imprimirTablero();
-        j.ponerFicha(t);
-        
-        
-    }
+    void prueba();
 };
 
 #endif
